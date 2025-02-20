7a1b222d11cbeb245e2411110c2a226f78741448
Hongyi Jin <jinhongyi02@gmail.com>
2025-02-20 16:31:40 -0500

    [Trn][Kernel] improve gemm perf by specifying allocation and pipeline (#124)

--- tests/python/tir+/kernels/trn/test_hgemm.py
+++ tests/python/tir+/kernels/trn/test_hgemm.py
@@ -33,12 +33,52 @@ def test_gemm(ssh_client):
     M = 4096
     N = 2048
     BLOCK_M = 2048
-    BLOCK_N = 256
+    BLOCK_N = 1024
     BLOCK_K = 1024
     NUM_BLOCK_M = M // BLOCK_M
     NUM_BLOCK_N = N // BLOCK_N
     NUM_BLOCK_K = K // BLOCK_K
+    TILE_M = 128
+    TILE_N = 512
+    NUM_TILE_M = BLOCK_M // TILE_M
+    NUM_TILE_N = BLOCK_N // TILE_N
     dtype = "float16"
+
+    # fmt: off
+    @T.macro
+    def mm(n, k, m, a_sbuf, b_sbuf, c_psum, result_tiles):
+        for mi, ni in T.grid(NUM_TILE_M, NUM_TILE_N):
+            psum_bank = T.meta_var((mi*NUM_TILE_N+ni)%8)
+            TILE_N_START = T.meta_var(ni * TILE_N)
+            TILE_M_START = T.meta_var(mi * TILE_M)
+            # FIXME: currently nki has a bug for psum initialization
+            # fix this when NKI exposes psum initialization API
+            Tp.gemm(c_psum[psum_bank], a_sbuf[m%2, TILE_M_START:TILE_M_START+TILE_M, :], b_sbuf[(n*NUM_BLOCK_K+k)%2, :, TILE_N_START:TILE_N_START+TILE_N], c_psum[psum_bank])
+            Tp.add(
+                result_tiles[
+                    m * BLOCK_M + TILE_M_START : m * BLOCK_M + TILE_M_START+TILE_M,
+                    TILE_N_START : TILE_N_START + TILE_N,
+                ],
+                result_tiles[
+                    m * BLOCK_M + TILE_M_START : m * BLOCK_M + TILE_M_START+TILE_M,
+                    TILE_N_START : TILE_N_START + TILE_N,
+                ],
+                c_psum[psum_bank],
+            )
+
+    @T.macro
+    def load_A(n, k, m, a_sbuf, A):
+        Tp.copy(
+            a_sbuf[m % 2], A[(m) * BLOCK_M : (m + 1) * BLOCK_M, k * BLOCK_K : (k + 1) * BLOCK_K]
+        )
+
+    @T.macro
+    def load_B(n, k, b_sbuf, B):
+        Tp.copy(
+            b_sbuf[(n * NUM_BLOCK_K + k) % 2],
+            B[(k) * BLOCK_K : (k + 1) * BLOCK_K, n * BLOCK_N : (n + 1) * BLOCK_N],
+        )
+
     # C = A.T @ B
     @T.prim_func(tirp=True)
     def matmul(A_ptr: T.handle, B_ptr: T.handle, C_ptr: T.handle):
@@ -50,20 +90,27 @@ def test_gemm(ssh_client):
         # todo: we should allow alloc_buffer under for loops
         # todo: can we skip the first T.kernel()?
         with T.kernel():
-            result_tiles = T.alloc_buffer((M, BLOCK_N), "float32", scope="trn.sbuf", layout=T.TrainiumLayout("FPF", T.TileLayout.from_tuple((M // 128, 128, BLOCK_N), (BLOCK_N, 1, 1))))
-            b_sbuf = T.alloc_buffer((BLOCK_K, BLOCK_N), dtype, scope="trn.sbuf", layout = T.TrainiumLayout("FPF", T.TileLayout.from_tuple((BLOCK_K // 128, 128, BLOCK_N), (BLOCK_N, 1, 1))))
-            a_sbuf = T.alloc_buffer((BLOCK_M, BLOCK_K), dtype, scope="trn.sbuf", layout = T.TrainiumLayout("FFP", T.TileLayout.from_tuple((BLOCK_M, BLOCK_K // 128, 128), (1, BLOCK_M, 1))))
-            c_psum = T.alloc_buffer((BLOCK_M, BLOCK_N), "float32", scope="trn.psum", layout = T.TrainiumPSUMLayout("FPF", T.TileLayout.from_tuple((BLOCK_M // 128, 128, BLOCK_N), (BLOCK_N, 1, 1))))
+            result_tiles = T.alloc_buffer((2, M, BLOCK_N), dtype, scope="trn.sbuf", layout=T.TrainiumLayout("FPF", T.TileLayout.from_tuple((M // 128, 128, BLOCK_N), (BLOCK_N, 1, 1))), allocated_addr=0)
+            b_sbuf = T.alloc_buffer((2, BLOCK_K, BLOCK_N), dtype, scope="trn.sbuf", layout = T.TrainiumLayout("FFPF", T.TileLayout.from_tuple((2, BLOCK_K // 128, 128, BLOCK_N), (BLOCK_K*BLOCK_N//128,BLOCK_N, 1, 1))), allocated_addr=M*BLOCK_N//128*2)
+            a_sbuf = T.alloc_buffer((2, BLOCK_M, BLOCK_K), dtype, scope="trn.sbuf", layout = T.TrainiumLayout("FFFP", T.TileLayout.from_tuple((2, BLOCK_M, BLOCK_K // 128, 128), (BLOCK_M*BLOCK_K//128, 1, BLOCK_M, 1))), allocated_addr=M*BLOCK_N//128*2 + 2*BLOCK_K*BLOCK_N//128*2)
+            c_psum = T.alloc_buffer((8, TILE_M, TILE_N), "float32", scope="trn.psum", layout = T.TrainiumPSUMLayout("FPF", T.TileLayout.from_tuple((8, TILE_M, TILE_N), (TILE_N, 1, 1))), allocated_addr=(0, 0))
             for n in T.serial(NUM_BLOCK_N):
                 Tp.memset(result_tiles, T.float32(0.0))
-                for k in T.serial(NUM_BLOCK_K):
-                    Tp.copy(b_sbuf, B[k * BLOCK_K : (k + 1) * BLOCK_K, n * BLOCK_N : (n + 1) * BLOCK_N])
-                    for m in T.serial(NUM_BLOCK_M):
-                        Tp.copy(a_sbuf, A[m * BLOCK_M : (m + 1) * BLOCK_M, k * BLOCK_K : (k + 1) * BLOCK_K])
-                        Tp.memset(c_psum, T.float32(0.0))
-                        Tp.gemm(c_psum, a_sbuf, b_sbuf, c_psum)
-                        Tp.add(result_tiles[m*BLOCK_M:(m+1)*BLOCK_M, :], result_tiles[m*BLOCK_M:(m+1)*BLOCK_M, :], c_psum)
+                load_B(n, 0, b_sbuf, B)
+                load_A(n, 0, 0, a_sbuf, A)
+                for k in T.serial(NUM_BLOCK_K-1):
+                    load_B(n, k+1, b_sbuf, B)
+                    load_A(n, k+1, 0, a_sbuf, A)
+                    for m in T.serial(NUM_BLOCK_M-1):
+                        load_A(n, k, m+1, a_sbuf, A)
+                        mm(n, k, m, a_sbuf, b_sbuf, c_psum, result_tiles)
+                    mm(n,k,NUM_BLOCK_M-1, a_sbuf, b_sbuf, c_psum, result_tiles)
+                for m in T.serial(NUM_BLOCK_M-1):
+                    load_A(n, NUM_BLOCK_K-1, m+1, a_sbuf, A)
+                    mm(n, NUM_BLOCK_K - 1, m, a_sbuf, b_sbuf, c_psum, result_tiles)
+                mm(n, NUM_BLOCK_K - 1, NUM_BLOCK_M - 1, a_sbuf, b_sbuf, c_psum, result_tiles)
                 Tp.copy(C[0:M, n*BLOCK_N:(n+1)*BLOCK_N], result_tiles)
+    # fmt: on
     with target:
         mod = tvm.IRModule({"main": matmul})
         mod = tvm.tir.transform.LowerTIRp()(mod)
