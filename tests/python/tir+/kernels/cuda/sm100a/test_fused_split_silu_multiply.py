01d3155c63ce5556ec0fc296b0c058c253f6e3be
Bohan Hou <bohanhou@andrew.cmu.edu>
2025-08-12 11:59:00 -0400

    kernel: symbolic shape split silu mul (#255)

--- tests/python/tir+/kernels/cuda/sm100a/test_fused_split_silu_multiply.py
+++ tests/python/tir+/kernels/cuda/sm100a/test_fused_split_silu_multiply.py
@@ -15,21 +15,17 @@
 # specific language governing permissions and limitations
 # under the License.
 import math
-import random
 import numpy as np
 import tvm
 from tvm.script import tir as T
 from tvm.script import tirp as Tp
 from ..utils import bench, ProtonContext
+import pytest
 
 
 def ceildiv(a, b):
     return (a + b - 1) // b
 
-
-# Problem config
-SEQ_LEN = 128
-INTERMEDIATE_SIZE = 14336
 
 # HW config
 SM_COUNT = 148
@@ -39,73 +35,88 @@ SMEM_SIZE = 232448
 F16_BYTE = 2
 F32_BYTE = 4
 
-# Kernel config
-VEC_SIZE = math.gcd(16 // F16_BYTE, INTERMEDIATE_SIZE)
-THREAD_NUM = min(256, INTERMEDIATE_SIZE // VEC_SIZE)
-BDX = 32
-BDY = THREAD_NUM // BDX
 
-
-def perpare_data():
+def perpare_data(batch_size, dim):
     import torch
 
     torch.manual_seed(42)
-    input_cat = torch.randn(SEQ_LEN, INTERMEDIATE_SIZE * 2).half()
+    input_cat = torch.randn(batch_size, dim * 2).half()
     return input_cat
 
 
-# fmt: off
-@T.prim_func(tirp=True)
-def fused_split_silu_multiply(input_cat_ptr: T.handle, output_ptr: T.handle):
-    input_cat_global = T.match_buffer(input_cat_ptr, [SEQ_LEN, INTERMEDIATE_SIZE * 2], "float16", scope="global", layout="default")
-    output_global = T.match_buffer(output_ptr, [SEQ_LEN, INTERMEDIATE_SIZE], "float16", scope="global", layout="default")
+def get_fused_split_silu_multiply_kernel(out_dim):
+    INTERMEDIATE_SIZE = out_dim
 
-    with T.kernel():
-        bx = T.cta_id([SM_COUNT], parent="kernel")
-        tx, ty = T.thread_id([BDX, BDY], parent="cta")
-        thread_id = T.meta_var(ty * BDX + tx)
-        
-        with T.thread():
-            idx = T.alloc_local([1], "int32", layout="default")
-            vec1 = T.alloc_local([VEC_SIZE], "float16", layout="default")
-            vec2 = T.alloc_local([VEC_SIZE], "float16", layout="default")
+    # Kernel config
+    VEC_SIZE = math.gcd(16 // F16_BYTE, INTERMEDIATE_SIZE)
+    THREAD_NUM = min(256, INTERMEDIATE_SIZE // VEC_SIZE)
+    BDX = 32
+    BDY = THREAD_NUM // BDX
+
+    # fmt: off
+    @T.prim_func(tirp=True)
+    def fused_split_silu_multiply(input_cat_ptr: T.handle, output_ptr: T.handle):
+        batch_size = T.int32()
+
+        input_cat_global = T.match_buffer(input_cat_ptr, [batch_size, INTERMEDIATE_SIZE * 2], "float16", scope="global", layout="default")    
+        output_global = T.match_buffer(output_ptr, [batch_size, INTERMEDIATE_SIZE], "float16", scope="global", layout="default")
+
+        with T.kernel():
+            bx = T.cta_id([SM_COUNT], parent="kernel")
+            tx, ty = T.thread_id([BDX, BDY], parent="cta")
+            thread_id = T.meta_var(ty * BDX + tx)
             
-            idx[0] = bx
-            while idx[0] < ceildiv(SEQ_LEN * INTERMEDIATE_SIZE , THREAD_NUM * VEC_SIZE):
-                real_idx = T.meta_var((idx[0] * THREAD_NUM + thread_id) * VEC_SIZE)
-                token_idx = T.meta_var(real_idx // INTERMEDIATE_SIZE)
-                offset_imme = T.meta_var((real_idx % INTERMEDIATE_SIZE) % INTERMEDIATE_SIZE)
-                for kv in T.serial(VEC_SIZE):
-                    vec1[kv] = input_cat_global[token_idx, offset_imme + kv]
-                for kv in T.serial(VEC_SIZE):
-                    vec2[kv] = input_cat_global[token_idx, INTERMEDIATE_SIZE + offset_imme + kv]
-                for kv in T.serial(VEC_SIZE):
-                    vec1[kv] = vec1[kv] * T.sigmoid(vec1[kv]) * vec2[kv]
-                for kv in T.serial(VEC_SIZE):
-                    output_global[token_idx, offset_imme + kv] = vec1[kv]
-                idx[0] += SM_COUNT
-
-@T.prim_func
-def fused_split1_silu1_multiply1(p_fastertransformer_gemm_fp16_int514: T.handle, p_output0: T.handle):
-    T.func_attr({"tir.is_scheduled": True, "tir.noalias": True})
-    seq_len = T.int64()
-    fastertransformer_gemm_fp16_int514 = T.match_buffer(p_fastertransformer_gemm_fp16_int514, (T.int64(1), seq_len, T.int64(INTERMEDIATE_SIZE * 2)), "float16")
-    T_multiply_intermediate_1 = T.match_buffer(p_output0, (T.int64(1), seq_len, T.int64(INTERMEDIATE_SIZE)), "float16")
-    # with T.block("root"):
-    for ax0_ax1_fused_0 in T.thread_binding(seq_len * T.int64(14), thread="blockIdx.x"):
-        for ax0_ax1_fused_1 in T.thread_binding(T.int64(1024), thread="threadIdx.x"):
-            with T.block("T_multiply_1"):
-                v0 = T.axis.spatial(seq_len, (ax0_ax1_fused_0 * T.int64(1024) + ax0_ax1_fused_1) // T.int64(INTERMEDIATE_SIZE))
-                v1 = T.axis.spatial(T.int64(INTERMEDIATE_SIZE), (ax0_ax1_fused_0 * T.int64(1024) + ax0_ax1_fused_1) % T.int64(INTERMEDIATE_SIZE))
-                T.reads(fastertransformer_gemm_fp16_int514[T.int64(0), v0, v1:v1 + T.int64(INTERMEDIATE_SIZE + 1)])
-                T.writes(T_multiply_intermediate_1[T.int64(0), v0, v1])
-                T_multiply_intermediate_1[T.int64(0), v0, v1] = fastertransformer_gemm_fp16_int514[T.int64(0), v0, v1] * T.sigmoid(fastertransformer_gemm_fp16_int514[T.int64(0), v0, v1]) * fastertransformer_gemm_fp16_int514[T.int64(0), v0, v1 + T.int64(INTERMEDIATE_SIZE)]
-# fmt: on
+            with T.thread():
+                idx = T.alloc_local([1], "int32", layout="default")
+                vec1 = T.alloc_local([VEC_SIZE], "float16", layout="default")
+                vec2 = T.alloc_local([VEC_SIZE], "float16", layout="default")
+                
+                idx[0] = bx
+                while idx[0] < ceildiv(batch_size * INTERMEDIATE_SIZE , THREAD_NUM * VEC_SIZE):
+                    real_idx = T.meta_var((idx[0] * THREAD_NUM + thread_id) * VEC_SIZE)
+                    token_idx = T.meta_var(real_idx // INTERMEDIATE_SIZE)
+                    offset_imme = T.meta_var((real_idx % INTERMEDIATE_SIZE) % INTERMEDIATE_SIZE)
+                    for kv in T.serial(VEC_SIZE):
+                        vec1[kv] = input_cat_global[token_idx, offset_imme + kv]
+                    for kv in T.serial(VEC_SIZE):
+                        vec2[kv] = input_cat_global[token_idx, INTERMEDIATE_SIZE + offset_imme + kv]
+                    for kv in T.serial(VEC_SIZE):
+                        vec1[kv] = vec1[kv] * T.sigmoid(vec1[kv]) * vec2[kv]
+                    for kv in T.serial(VEC_SIZE):
+                        output_global[token_idx, offset_imme + kv] = vec1[kv]
+                    idx[0] += SM_COUNT
+    # fmt: on
+    return fused_split_silu_multiply
 
 
-def test():
+def get_fused_split1_silu1_multiply1_kernel(out_dim):
+    INTERMEDIATE_SIZE = out_dim
 
-    input_cat = perpare_data()
+    # fmt: off
+    @T.prim_func
+    def fused_split1_silu1_multiply1(p_fastertransformer_gemm_fp16_int514: T.handle, p_output0: T.handle):
+        T.func_attr({"tir.is_scheduled": True, "tir.noalias": True})
+        seq_len = T.int64()
+        fastertransformer_gemm_fp16_int514 = T.match_buffer(p_fastertransformer_gemm_fp16_int514, (T.int64(1), seq_len, T.int64(INTERMEDIATE_SIZE * 2)), "float16")
+        T_multiply_intermediate_1 = T.match_buffer(p_output0, (T.int64(1), seq_len, T.int64(INTERMEDIATE_SIZE)), "float16")
+        # with T.block("root"):
+        for ax0_ax1_fused_0 in T.thread_binding(seq_len * T.int64(INTERMEDIATE_SIZE // 1024), thread="blockIdx.x"):
+            for ax0_ax1_fused_1 in T.thread_binding(T.int64(1024), thread="threadIdx.x"):
+                with T.block("T_multiply_1"):
+                    v0 = T.axis.spatial(seq_len, (ax0_ax1_fused_0 * T.int64(1024) + ax0_ax1_fused_1) // T.int64(INTERMEDIATE_SIZE))
+                    v1 = T.axis.spatial(T.int64(INTERMEDIATE_SIZE), (ax0_ax1_fused_0 * T.int64(1024) + ax0_ax1_fused_1) % T.int64(INTERMEDIATE_SIZE))
+                    T.reads(fastertransformer_gemm_fp16_int514[T.int64(0), v0, v1:v1 + T.int64(INTERMEDIATE_SIZE + 1)])
+                    T.writes(T_multiply_intermediate_1[T.int64(0), v0, v1])
+                    T_multiply_intermediate_1[T.int64(0), v0, v1] = fastertransformer_gemm_fp16_int514[T.int64(0), v0, v1] * T.sigmoid(fastertransformer_gemm_fp16_int514[T.int64(0), v0, v1]) * fastertransformer_gemm_fp16_int514[T.int64(0), v0, v1 + T.int64(INTERMEDIATE_SIZE)]
+
+    # fmt: on
+    return fused_split1_silu1_multiply1
+
+
+@pytest.mark.parametrize("batch_size", [1, 2, 4, 8, 16, 32, 64, 128])
+def test(batch_size):
+    out_dim = 25600
+    input_cat = perpare_data(batch_size, out_dim)
 
     def naive():
         import torch
@@ -113,8 +124,8 @@ def test():
         input_cat_naive = input_cat.clone().to("cuda")
 
         def func():
-            return input_cat_naive[..., INTERMEDIATE_SIZE:] * torch.nn.functional.silu(
-                input_cat_naive[..., :INTERMEDIATE_SIZE]
+            return input_cat_naive[..., out_dim:] * torch.nn.functional.silu(
+                input_cat_naive[..., :out_dim]
             )
 
         ms = bench(func, warmup=0, repeat=30, proton_name="naive")
@@ -125,12 +136,10 @@ def test():
     def tir():
         DEV = tvm.cuda(0)
         input_cat_tvm = tvm.nd.array(input_cat.clone(), device=DEV)
-        output_tvm = tvm.nd.array(
-            np.zeros((SEQ_LEN, INTERMEDIATE_SIZE), dtype=np.float16), device=DEV
-        )
+        output_tvm = tvm.nd.array(np.zeros((batch_size, out_dim), dtype=np.float16), device=DEV)
         target = tvm.target.Target("cuda")
         with target:
-            mod = tvm.IRModule({"main": fused_split_silu_multiply})
+            mod = tvm.IRModule({"main": get_fused_split_silu_multiply_kernel(out_dim)})
             mod = tvm.compile(mod, target=target, tir_pipeline="tirp")
             func = lambda: mod(input_cat_tvm, output_tvm)
             ms = bench(func, warmup=0, repeat=30, proton_name="tir")
@@ -141,20 +150,18 @@ def test():
     def tir2():
         DEV = tvm.cuda(0)
         input_cat_tvm = tvm.nd.array(
-            input_cat.clone().reshape(1, SEQ_LEN, INTERMEDIATE_SIZE * 2), device=DEV
+            input_cat.clone().reshape(1, batch_size, out_dim * 2), device=DEV
         )
-        output_tvm = tvm.nd.array(
-            np.zeros((1, SEQ_LEN, INTERMEDIATE_SIZE), dtype=np.float16), device=DEV
-        )
+        output_tvm = tvm.nd.array(np.zeros((1, batch_size, out_dim), dtype=np.float16), device=DEV)
         target = tvm.target.Target("cuda")
         with target:
-            mod = tvm.IRModule({"main": fused_split1_silu1_multiply1})
+            mod = tvm.IRModule({"main": get_fused_split1_silu1_multiply1_kernel(out_dim)})
             mod = tvm.compile(mod, target=target)
             func = lambda: mod(input_cat_tvm, output_tvm)
             ms = bench(func, warmup=0, repeat=30, proton_name="tir2")
             print(f"TIR time: {ms:.3f} ms")
 
-        return output_tvm.numpy().reshape(SEQ_LEN, INTERMEDIATE_SIZE)
+        return output_tvm.numpy().reshape(batch_size, out_dim)
 
     with ProtonContext("fused_split_silu_multiply"):
         output_naive = naive()
@@ -166,4 +173,5 @@ def test():
 
 
 if __name__ == "__main__":
-    test()
+    for batch_size in [64, 128]:
+        test(batch_size)
