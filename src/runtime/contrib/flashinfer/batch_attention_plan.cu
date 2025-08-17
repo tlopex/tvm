6773bbb2cd4045cd6420b6da54f9ed7de3093649
Ruihang Lai <ruihangl@cs.cmu.edu>
2025-08-16 23:30:41 -0400

    Update FlashInfer BatchPagedAttentionPlan (#275)

--- src/runtime/contrib/flashinfer/batch_attention_plan.cu
+++ src/runtime/contrib/flashinfer/batch_attention_plan.cu
@@ -32,6 +32,7 @@
 #include "../../../../3rdparty/flashinfer/include/flashinfer/pos_enc.cuh"
 
 using IdType = int32_t;
+using tvm::ffi::Any;
 using tvm::ffi::Array;
 using tvm::runtime::DataType;
 using tvm::runtime::IntTuple;
@@ -183,11 +184,11 @@ Array<NDArray> BatchDecodeWithPagedKVCachePlan(
   return {request_indices, kv_tile_indices, kv_chunk_size_ptr};
 }
 
-IntTuple BatchPagedAttentionPlan(DLTensor* float_workspace_buffer, DLTensor* int_workspace_buffer,
-                                 DLTensor* page_locked_int_workspace_buffer, DLTensor* qo_indptr,
-                                 DLTensor* kv_indptr, DLTensor* kv_len, int64_t batch_size,
-                                 int64_t num_qo_heads, int64_t num_kv_heads, int64_t head_dim_o,
-                                 bool causal) {
+Array<Any> BatchPagedAttentionPlan(NDArray float_workspace_buffer, NDArray int_workspace_buffer,
+                                   NDArray page_locked_int_workspace_buffer, NDArray qo_indptr,
+                                   NDArray kv_indptr, NDArray kv_len, int64_t batch_size,
+                                   int64_t num_qo_heads, int64_t num_kv_heads, int64_t head_dim_o,
+                                   bool causal) {
   size_t float_workspace_size_in_bytes =
       float_workspace_buffer->shape[0] * DataType(float_workspace_buffer->dtype).bytes();
   size_t int_workspace_size_in_bytes =
@@ -198,6 +199,9 @@ IntTuple BatchPagedAttentionPlan(DLTensor* float_workspace_buffer, DLTensor* int
   const cudaStream_t stream =
       static_cast<cudaStream_t>(DeviceAPI::Get(device)->GetCurrentStream(device));
 
+  constexpr uint32_t NUM_TASKS = 2;
+  const uint32_t CTA_TILE_Q_SIZES[NUM_TASKS] = {128, 16};
+  int num_sm = 0;
   cudaError_t status = TwoStageHolisticPlan<IdType>(
       static_cast<char*>(float_workspace_buffer->data) + float_workspace_buffer->byte_offset,
       float_workspace_size_in_bytes,
@@ -208,13 +212,110 @@ IntTuple BatchPagedAttentionPlan(DLTensor* float_workspace_buffer, DLTensor* int
       static_cast<IdType*>(qo_indptr->data) + qo_indptr->byte_offset / sizeof(IdType),
       static_cast<IdType*>(kv_indptr->data) + kv_indptr->byte_offset / sizeof(IdType),
       static_cast<IdType*>(kv_len->data) + kv_len->byte_offset / sizeof(IdType), batch_size,
-      num_qo_heads, num_kv_heads, head_dim_o, causal, stream);
+      num_qo_heads, num_kv_heads, head_dim_o, causal, stream, &num_sm);
 
   CHECK(status == cudaSuccess) << "BatchPagedAttentionPlan failed with error "
                                << cudaGetErrorString(status);
 
+  constexpr uint32_t NUM_TASK_ARGS = 11;
+  constexpr uint32_t NUM_SHARED_ARGS = 7;
+  const int max_total_num_works = 65536;
+  int cluster_size = 1;
+  int num_clusters = num_sm / cluster_size;
+  const int max_num_kv_splits =
+      4 * num_clusters * cluster_size * (CTA_TILE_Q_SIZES[0] + CTA_TILE_Q_SIZES[1]);
+  DataType int_dtype = DataType::Int(32);
+  DataType fp16_dtype = DataType::Float(16);
+  DataType fp32_dtype = DataType::Float(32);
+
   std::vector<int64_t> plan_info_vec = plan_info.ToVector();
-  return IntTuple{plan_info_vec.begin(), plan_info_vec.end()};
+  CHECK_EQ(plan_info_vec.size(), NUM_SHARED_ARGS + NUM_TASKS * NUM_TASK_ARGS);
+
+  std::vector<Any> ret;
+  int64_t num_blks_x = plan_info_vec[0];
+  int64_t num_blks_y = plan_info_vec[1];
+  ret.push_back(num_blks_x);
+  ret.push_back(num_blks_y);
+
+  for (uint32_t i = 0; i < NUM_TASKS; ++i) {
+    std::vector<NDArray> task_arrays;
+    task_arrays.reserve(NUM_TASK_ARGS);
+    int64_t q_indptr_offset = plan_info_vec[2 + i * NUM_TASK_ARGS + 0];
+    NDArray q_indptr = int_workspace_buffer.CreateView({max_total_num_works}, int_dtype,
+                                                       /*relative_byte_offset=*/q_indptr_offset);
+    task_arrays.push_back(q_indptr);
+    int64_t kv_indptr_offset = plan_info_vec[2 + i * NUM_TASK_ARGS + 1];
+    NDArray kv_indptr = int_workspace_buffer.CreateView({max_total_num_works}, int_dtype,
+                                                        /*relative_byte_offset=*/kv_indptr_offset);
+    task_arrays.push_back(kv_indptr);
+    int64_t partial_indptr_offset = plan_info_vec[2 + i * NUM_TASK_ARGS + 2];
+    NDArray partial_indptr =
+        int_workspace_buffer.CreateView({max_total_num_works}, int_dtype,
+                                        /*relative_byte_offset=*/partial_indptr_offset);
+    task_arrays.push_back(partial_indptr);
+    int64_t q_len_offset = plan_info_vec[2 + i * NUM_TASK_ARGS + 3];
+    NDArray q_len = int_workspace_buffer.CreateView({max_total_num_works}, int_dtype,
+                                                    /*relative_byte_offset=*/q_len_offset);
+    task_arrays.push_back(q_len);
+    int64_t kv_len_offset = plan_info_vec[2 + i * NUM_TASK_ARGS + 4];
+    NDArray kv_len = int_workspace_buffer.CreateView({max_total_num_works}, int_dtype,
+                                                     /*relative_byte_offset=*/kv_len_offset);
+    task_arrays.push_back(kv_len);
+    int64_t q_start_offset = plan_info_vec[2 + i * NUM_TASK_ARGS + 5];
+    NDArray q_start = int_workspace_buffer.CreateView({max_total_num_works}, int_dtype,
+                                                      /*relative_byte_offset=*/q_start_offset);
+    task_arrays.push_back(q_start);
+    int64_t kv_start_offset = plan_info_vec[2 + i * NUM_TASK_ARGS + 6];
+    NDArray kv_start = int_workspace_buffer.CreateView({max_total_num_works}, int_dtype,
+                                                       /*relative_byte_offset=*/kv_start_offset);
+    task_arrays.push_back(kv_start);
+    int64_t kv_end_offset = plan_info_vec[2 + i * NUM_TASK_ARGS + 7];
+    NDArray kv_end = int_workspace_buffer.CreateView({max_total_num_works}, int_dtype,
+                                                     /*relative_byte_offset=*/kv_end_offset);
+    task_arrays.push_back(kv_end);
+    int64_t kv_head_idx_offset = plan_info_vec[2 + i * NUM_TASK_ARGS + 8];
+    NDArray kv_head_idx =
+        int_workspace_buffer.CreateView({max_total_num_works}, int_dtype,
+                                        /*relative_byte_offset=*/kv_head_idx_offset);
+    task_arrays.push_back(kv_head_idx);
+    int64_t work_indptr_offset = plan_info_vec[2 + i * NUM_TASK_ARGS + 9];
+    NDArray work_indptr =
+        int_workspace_buffer.CreateView({max_total_num_works}, int_dtype,
+                                        /*relative_byte_offset=*/work_indptr_offset);
+    task_arrays.push_back(work_indptr);
+    int64_t len_kv_chunk_offset = plan_info_vec[2 + i * NUM_TASK_ARGS + 10];
+    NDArray len_kv_chunk =
+        int_workspace_buffer.CreateView({max_total_num_works}, int_dtype,
+                                        /*relative_byte_offset=*/len_kv_chunk_offset);
+    task_arrays.push_back(len_kv_chunk);
+    ret.push_back(Array<NDArray>(task_arrays));
+  }
+
+  int64_t partial_o_offset = plan_info_vec[2 + NUM_TASKS * NUM_TASK_ARGS];
+  NDArray partial_o =
+      float_workspace_buffer.CreateView({2 * max_num_kv_splits * head_dim_o}, fp16_dtype,
+                                        /*relative_byte_offset=*/partial_o_offset);
+  ret.push_back(partial_o);
+  int64_t partial_lse_offset = plan_info_vec[3 + NUM_TASKS * NUM_TASK_ARGS];
+  NDArray partial_lse =
+      float_workspace_buffer.CreateView({2 * max_num_kv_splits}, fp32_dtype,
+                                        /*relative_byte_offset=*/partial_lse_offset);
+  ret.push_back(partial_lse);
+  int64_t merge_indptr_offset = plan_info_vec[4 + NUM_TASKS * NUM_TASK_ARGS];
+  NDArray merge_indptr =
+      int_workspace_buffer.CreateView({max_num_kv_splits}, int_dtype,
+                                      /*relative_byte_offset=*/merge_indptr_offset);
+  ret.push_back(merge_indptr);
+  int64_t merge_o_indices_offset = plan_info_vec[5 + NUM_TASKS * NUM_TASK_ARGS];
+  NDArray merge_o_indices =
+      int_workspace_buffer.CreateView({max_num_kv_splits}, int_dtype,
+                                      /*relative_byte_offset=*/merge_o_indices_offset);
+  ret.push_back(merge_o_indices);
+  int64_t num_qo_len_offset = plan_info_vec[6 + NUM_TASKS * NUM_TASK_ARGS];
+  NDArray num_qo_len = int_workspace_buffer.CreateView({1}, int_dtype,
+                                                       /*relative_byte_offset=*/num_qo_len_offset);
+  ret.push_back(num_qo_len);
+  return ret;
 }
 
 TVM_FFI_STATIC_INIT_BLOCK({
