595b3b9d850e16171b9c8a2c844d125ab3d3bc0b
Ruihang Lai <ruihangl@cs.cmu.edu>
2025-08-19 01:55:36 -0400

    Add megakernel utils (#283)

---
+++ src/runtime/vm/megakernel_utils.cc
@@ -0,0 +1,224 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include "megakernel_utils.h"
+
+#include <tvm/ffi/container/array.h>
+#include <tvm/ffi/container/shape.h>
+#include <tvm/ffi/function.h>
+#include <tvm/ffi/reflection/registry.h>
+#include <tvm/runtime/ndarray.h>
+#include <tvm/runtime/nvtx.h>
+
+#include <utility>
+
+namespace tvm {
+namespace runtime {
+namespace vm {
+namespace megakernel {
+
+using ffi::Array;
+using ffi::Shape;
+using runtime::NDArray;
+
+Array<NDArray> GetEventTensorsOnLayer(Array<NDArray> etensors, int layer_id) {
+  TVM_FFI_ICHECK_GE(layer_id, 0) << "Layer id must be non-negative, but got " << layer_id;
+  TVM_FFI_ICHECK_EQ(etensors.size(), 15) << "Event tensors size must be 15";
+  std::vector<NDArray> etensors_on_layer;
+  etensors_on_layer.reserve(etensors.size());
+  for (int i = 0; i < static_cast<int>(etensors.size()); i++) {
+    Shape shape = etensors[i].shape();
+    TVM_FFI_ICHECK_GE(shape.size(), 2) << "Event tensor must have at least 2 dimensions";
+    TVM_FFI_ICHECK_LT(layer_id, shape[0])
+        << "Layer id must be less than the first dimension of the event tensor, but got "
+        << layer_id << " and " << shape[0];
+    std::vector<int64_t> new_shape_vec{shape.begin() + 1, shape.end()};
+    Shape new_shape(new_shape_vec);
+    int64_t offset = layer_id * new_shape.Product() * DataType(etensors[i].dtype()).bytes();
+    etensors_on_layer.push_back(etensors[i].CreateView(new_shape, etensors[i].dtype(), offset));
+  }
+  return Array<NDArray>(etensors_on_layer);
+}
+
+NDArray GenerateExecQueue(int batch_size, int new_batch_size, int tp_size, int num_qo_heads,
+                          int num_kv_heads, int head_dim, Device device,
+                          Device preferred_host_device) {
+  NVTXScopedRange range("Generate execution queue");
+  bool split_kv = batch_size != new_batch_size;
+
+  NDArray exec_queue_host = NDArray::Empty({kNumSM, kStaticTileSchedulerMaxTasks, kTaskSize},
+                                           DataType::Int(32), preferred_host_device);
+  NDArray exec_queue_device =
+      NDArray::Empty({kNumSM, kStaticTileSchedulerMaxTasks, kTaskSize}, DataType::Int(32), device);
+  int32_t* exec_queue_host_data = static_cast<int32_t*>(exec_queue_host->data);
+
+  // Generate round-robin static execution queue
+  std::vector<int32_t> task_counts(kNumSM, 0);
+  int cur_sm = 0;
+
+  auto f_push_task = [&exec_queue_host_data, &task_counts, &cur_sm](int m_idx, int n_idx, int k_idx,
+                                                                    JobType job_type) {
+    int task_id = task_counts[cur_sm]++;
+    // clang-format off
+    exec_queue_host_data[cur_sm * kStaticTileSchedulerMaxTasks * kTaskSize + task_id * kTaskSize + 0] = m_idx;
+    exec_queue_host_data[cur_sm * kStaticTileSchedulerMaxTasks * kTaskSize + task_id * kTaskSize + 1] = n_idx;
+    exec_queue_host_data[cur_sm * kStaticTileSchedulerMaxTasks * kTaskSize + task_id * kTaskSize + 2] = k_idx;
+    exec_queue_host_data[cur_sm * kStaticTileSchedulerMaxTasks * kTaskSize + task_id * kTaskSize + 3] = static_cast<int32_t>(job_type);
+    // clang-format on
+    cur_sm = (cur_sm + 1) % kNumSM;
+  };
+
+  for (int n_idx = 0; n_idx < ceildiv((num_qo_heads + 2 * num_kv_heads) * head_dim, kGemmTileBlkN);
+       ++n_idx) {
+    for (int k_idx = 0; k_idx < kSplitQKVProject; ++k_idx) {
+      f_push_task(0, n_idx, k_idx, JobType::kGemmQKVProj);
+    }
+  }
+
+  int32_t m_split = std::min(batch_size, ceildiv(kNumSM, (num_qo_heads + 2 * num_kv_heads) *
+                                                             head_dim / kSplitKReduceTileNUnit));
+  int32_t n_tile_qkv_proj_reduce =
+      (ceildiv(kSplitKReduceTileNRepeat, ceildiv(batch_size, m_split)) * kSplitKReduceTileNUnit);
+  for (int m_idx = 0; m_idx < m_split; ++m_idx) {
+    for (int n_idx = 0;
+         n_idx < ceildiv((num_qo_heads + 2 * num_kv_heads) * head_dim, n_tile_qkv_proj_reduce);
+         ++n_idx) {
+      f_push_task(m_idx, n_idx, 0, JobType::kGemmQKVReduce);
+    }
+  }
+
+  for (int m_idx = 0; m_idx < m_split; ++m_idx) {
+    for (int n_idx = 0; n_idx < num_qo_heads; ++n_idx) {
+      f_push_task(m_idx, n_idx, -1, JobType::kQRMSNormRope);
+    }
+  }
+
+  for (int m_idx = 0; m_idx < m_split; ++m_idx) {
+    for (int n_idx = 0; n_idx < num_kv_heads; ++n_idx) {
+      f_push_task(m_idx, n_idx, -1, JobType::kKRMSNormRopeAppendKV);
+    }
+  }
+
+  for (int m_idx = 0; m_idx < m_split; ++m_idx) {
+    for (int n_idx = 0; n_idx < num_kv_heads; ++n_idx) {
+      f_push_task(m_idx, n_idx, -1, JobType::kVAppendKV);
+    }
+  }
+
+  if (split_kv) {
+    for (int n_idx = 0; n_idx < num_kv_heads; ++n_idx) {
+      for (int m_idx = 0; m_idx < new_batch_size; ++m_idx) {
+        f_push_task(m_idx, n_idx, -1, JobType::kBatchDecodeSplit);
+      }
+    }
+  } else {
+    for (int n_idx = 0; n_idx < num_kv_heads; ++n_idx) {
+      for (int m_idx = 0; m_idx < batch_size; ++m_idx) {
+        f_push_task(m_idx, n_idx, -1, JobType::kBatchDecodeNoSplit);
+      }
+    }
+  }
+
+  if (split_kv) {
+    for (int n_idx = 0; n_idx < num_kv_heads; ++n_idx) {
+      for (int m_idx = 0; m_idx < batch_size; ++m_idx) {
+        f_push_task(m_idx, n_idx, -1, JobType::kDecodeMerge);
+      }
+    }
+  }
+
+  for (int n_idx = 0; n_idx < ceildiv(kHiddenSize, kGemmTileBlkN); ++n_idx) {
+    for (int k_idx = 0; k_idx < kSplitOProject; ++k_idx) {
+      f_push_task(0, n_idx, k_idx, JobType::kGemmOProj);
+    }
+  }
+
+  int32_t m_split_o_proj_reduce =
+      std::min(batch_size, ceildiv(kNumSM, kHiddenSize / kSplitKReduceTileNUnit));
+  int32_t n_tile_o_proj_reduce =
+      (ceildiv(kSplitKReduceTileNRepeat, ceildiv(batch_size, m_split_o_proj_reduce)) *
+       kSplitKReduceTileNUnit);
+  for (int n_idx = 0; n_idx < ceildiv(kHiddenSize, n_tile_o_proj_reduce); ++n_idx) {
+    for (int m_idx = 0; m_idx < m_split_o_proj_reduce; ++m_idx) {
+      f_push_task(m_idx, n_idx, 0, JobType::kGemmOReduce);
+    }
+  }
+
+  for (int m_idx = 0; m_idx < batch_size; ++m_idx) {
+    f_push_task(m_idx, -1, -1, JobType::kAttnAddRMSNorm);
+  }
+
+  for (int n_idx = 0; n_idx < ceildiv(kIntermediateSizeTP1 / tp_size * 2, kGemmTileBlkN); ++n_idx) {
+    f_push_task(0, n_idx, 0, JobType::kGemmGateUpProj);
+  }
+
+  for (int n_idx = 0; n_idx < ceildiv(kIntermediateSizeTP1 / tp_size, kSiluMultiplyTileTileSize);
+       ++n_idx) {
+    f_push_task(n_idx, 0, 0, JobType::kSplitSiluMultiply);
+  }
+
+  for (int n_idx = 0; n_idx < ceildiv(kHiddenSize, kGemmTileBlkN); ++n_idx) {
+    for (int k_idx = 0; k_idx < kDownProjSplitKFactor; ++k_idx) {
+      f_push_task(0, n_idx, k_idx, JobType::kGemmDownProj);
+    }
+  }
+
+  int32_t m_split_down_proj_reduce =
+      std::min(ceildiv(kNumSM, ceildiv(kHiddenSize, kSplitKReduceTileNUnit)), batch_size);
+  int32_t n_tile_down_proj_reduce =
+      (ceildiv(kSplitKReduceTileNRepeat, ceildiv(batch_size, m_split_down_proj_reduce)) *
+       kSplitKReduceTileNUnit);
+  for (int m_idx = 0; m_idx < m_split_down_proj_reduce; ++m_idx) {
+    for (int n_idx = 0; n_idx < ceildiv(kHiddenSize, n_tile_down_proj_reduce); ++n_idx) {
+      f_push_task(m_idx, n_idx, 0, JobType::kDownProjReduce);
+    }
+  }
+
+  for (int m_idx = 0; m_idx < batch_size; ++m_idx) {
+    f_push_task(m_idx, 0, 0, JobType::kMLPAddRMSNorm);
+  }
+
+  // Set the uninitialized queue to kEnd
+  for (int sm_id = 0; sm_id < kNumSM; ++sm_id) {
+    for (int task_id = task_counts[sm_id]; task_id < kStaticTileSchedulerMaxTasks; ++task_id) {
+      // clang-format off
+      exec_queue_host_data[sm_id * kStaticTileSchedulerMaxTasks * kTaskSize + task_id * kTaskSize + 0] = -1;
+      exec_queue_host_data[sm_id * kStaticTileSchedulerMaxTasks * kTaskSize + task_id * kTaskSize + 1] = -1;
+      exec_queue_host_data[sm_id * kStaticTileSchedulerMaxTasks * kTaskSize + task_id * kTaskSize + 2] = -1;
+      exec_queue_host_data[sm_id * kStaticTileSchedulerMaxTasks * kTaskSize + task_id * kTaskSize + 3] = static_cast<int32_t>(JobType::kEnd);
+      // clang-format on
+    }
+  }
+
+  // Transfer to device
+  DLTensor exec_queue_device_dl = *exec_queue_device.operator->();
+  NDArray::CopyFromTo(exec_queue_host.operator->(), &exec_queue_device_dl);
+  return exec_queue_device;
+}
+
+// RNN State methods
+TVM_FFI_STATIC_INIT_BLOCK({
+  namespace refl = tvm::ffi::reflection;
+  refl::GlobalDef().def("megakernel.get_event_tensors_on_layer", GetEventTensorsOnLayer);
+});
+
+}  // namespace megakernel
+}  // namespace vm
+}  // namespace runtime
+}  // namespace tvm
---
+++ src/runtime/vm/megakernel_utils.h
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*!
+ * \file src/runtime/vm/attn_utils.h
+ * \brief Data structure and utilities for KV cache.
+ */
+
+#ifndef TVM_RUNTIME_VM_MEGAKERNEL_UTILS_H_
+#define TVM_RUNTIME_VM_MEGAKERNEL_UTILS_H_
+
+#include <tvm/runtime/ndarray.h>
+
+namespace tvm {
+namespace runtime {
+namespace vm {
+
+inline int ceildiv(int a, int b) { return (a + b - 1) / b; }
+
+namespace megakernel {
+
+constexpr const int kSplitKReduceTileNUnit = 128;
+constexpr const int kSplitKReduceTileNRepeat = 1;
+constexpr const int kGemmTileBlkN = 128;
+constexpr const int kGemmTileBlkK = 64;
+constexpr const int kDownProjSplitKFactor = 10;
+constexpr const int kSplitQKVProject = 3;
+constexpr const int kSplitOProject = 3;
+constexpr const int kSiluMultiplyTileTileSize = 128;
+constexpr const int kHiddenSize = 5120;
+constexpr const int kNumAttentionHeadsTP1 = 64;
+constexpr const int kIntermediateSizeTP1 = 25600;
+
+constexpr const int kNumSM = 148;
+constexpr const int kStaticTileSchedulerMaxTasks = 128;
+constexpr const int kTaskSize = 4;
+
+enum class JobType : int32_t {
+  kGemmGateUpProj = 0,
+  kSplitSiluMultiply = 1,
+  kGemmDownProj = 2,
+  kDownProjReduce = 3,
+  kMLPAddRMSNorm = 4,
+  kGemmQKVProj = 5,
+  kGemmQKVReduce = 6,
+  kRMSNorm = 7,
+  kRope = 8,
+  kAppendKV = 9,
+  kBatchDecodeNoSplit = 10,
+  kBatchDecodeSplit = 11,
+  kDecodeMerge = 12,
+  kGemmOProj = 13,
+  kGemmOReduce = 14,
+  kAttnAddRMSNorm = 15,
+  kKRMSNormRopeAppendKV = 16,
+  kQRMSNormRope = 17,
+  kVAppendKV = 18,
+  kEnd = 99,
+};
+
+NDArray GenerateExecQueue(int batch_size, int new_batch_size, int tp_size, int num_qo_heads,
+                          int num_kv_heads, int head_dim, Device device,
+                          Device preferred_host_device);
+
+}  // namespace megakernel
+}  // namespace vm
+}  // namespace runtime
+}  // namespace tvm
+
+#endif  // TVM_RUNTIME_VM_MEGAKERNEL_UTILS_H_
